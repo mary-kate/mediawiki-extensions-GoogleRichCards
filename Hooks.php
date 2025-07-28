97d696eb9c7b686e9fc27a28515897272a357acc
Jack Phoenix <ashley@uncyclomedia.co>
2025-07-28 12:19:44 +0300

    Stylize + drop PHP ending tags from pure PHP files
    Messy diff but no functional changes are intended.

--- Hooks.php
+++ Hooks.php
@@ -6,10 +6,10 @@
  * PHP version 5.4
  *
  * @category Extension
- * @package  GoogleRichCards
- * @author   Igor Shishkin <me@teran.ru>
- * @license  GPL http://www.gnu.org/licenses/gpl.html
- * @link     https://github.com/teran/mediawiki-GoogleRichCards
+ * @package GoogleRichCards
+ * @author Igor Shishkin <me@teran.ru>
+ * @license GPL http://www.gnu.org/licenses/gpl.html
+ * @link https://github.com/teran/mediawiki-GoogleRichCards
  */
 
 namespace MediaWiki\Extension\GoogleRichCards;
@@ -19,49 +19,47 @@ use Parser;
 use Skin;
 
 class Hooks {
-  /**
+	/**
 	 * Handle meta elements and page title modification.
 	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
 	 * @param OutputPage &$out The output page.
 	 * @param Skin &$skin The current skin.
 	 * @return bool
 	 */
-  public static function onBeforePageDisplay(OutputPage &$out, Skin &$skin) {
-    global $wgGoogleRichCardsAnnotateArticles, $wgGoogleRichCardsAnnotateEvents, $wgGoogleRichCardsAnnotateWebSite;
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		global $wgGoogleRichCardsAnnotateArticles, $wgGoogleRichCardsAnnotateEvents, $wgGoogleRichCardsAnnotateWebSite;
 
-    if($wgGoogleRichCardsAnnotateArticles) {
-      $article = Article::getInstance();
-      $article->render($out);
-    }
+		if ( $wgGoogleRichCardsAnnotateArticles ) {
+			$article = Article::getInstance();
+			$article->render( $out );
+		}
 
-    if($wgGoogleRichCardsAnnotateEvents) {
-      $event = Event::getInstance();
-      $event->render($out);
-    }
+		if ( $wgGoogleRichCardsAnnotateEvents ) {
+			$event = Event::getInstance();
+			$event->render( $out );
+		}
 
-    if($wgGoogleRichCardsAnnotateWebSite) {
-      $website = WebSite::getInstance();
-      $website->render($out);
-    }
+		if ( $wgGoogleRichCardsAnnotateWebSite ) {
+			$website = WebSite::getInstance();
+			$website->render( $out );
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  /**
-   * Handle invocation of article parser
-   *
-   * @link https://www.mediawiki.org/wiki/Manual:Hooks/ParserFirstCallInit
-   * @param Parser &$parser The global parser.
-   */
-  public static function onParserFirstCallInit(Parser &$parser) {
-    global $wgGoogleRichCardsAnnotateEvents;
+	/**
+	 * Handle invocation of article parser
+	 *
+	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/ParserFirstCallInit
+	 * @param Parser &$parser The global parser.
+	 */
+	public static function onParserFirstCallInit( Parser &$parser ) {
+		global $wgGoogleRichCardsAnnotateEvents;
 
-    if($wgGoogleRichCardsAnnotateEvents) {
-      $event = Event::getInstance();
-      $parser->setHook('event', [$event, 'parse']);
-    }
-  }
+		if ( $wgGoogleRichCardsAnnotateEvents ) {
+			$event = Event::getInstance();
+			$parser->setHook( 'event', [ $event, 'parse' ] );
+		}
+	}
 
 }
-
-?>
